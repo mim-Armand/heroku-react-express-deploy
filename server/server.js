33a51e60b9622f2b3cc5bfc500e9c5bd1e14c6e2
mim <mim3dot@gmail.com>
2022-11-01 19:48:06 -0500

    Adding the path to the build directory in the client

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ if (process.env.NODE_ENV === 'production') {
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 // });
+app.use(express.static('public'));
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
