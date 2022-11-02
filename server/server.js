e110e1f7f6445d2b28645d4c7a80e7923674d4fe
mim <mim3dot@gmail.com>
2022-11-01 19:23:51 -0500

    Adding the path to the build directory in the client

--- server/server.js
+++ server/server.js
@@ -19,16 +19,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-  
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -36,7 +36,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
   };
-  
+
 // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
- 
+
